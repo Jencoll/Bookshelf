8574fada9abe70af706f74677c8e9bfede4a4552
Jennyfer Collin <info@jennyfercollin.com>
2022-04-15 20:48:30 -0400

    styling book thumbnails

--- client/src/components/Books/BookThumbnail.js
+++ client/src/components/Books/BookThumbnail.js
@@ -22,11 +22,32 @@ const BookThumbnail = ({displayBook}) => {
 };
 
 const BookLi = styled.li`
+  position: relative;
   padding: 12px;
   cursor: pointer;
   width: 180px;
   display: flex;
   align-items: center;
+  /* box-shadow: 0 1px 4px rgb(0 0 0 / 10%), 0 0 40px rgb(0 0 0 / 1%) inset; */
+
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    box-shadow: 0 0 10px rgb(0 0 0 / 20%);
+    top: 5%;
+    bottom: 5%;
+    left: 5%;
+    right: 5%;
+    border-radius: 10px / 10px;
+  }
+
+  &::after {
+    right: 10px;
+    left: auto;
+    transform: skew(8deg) rotate(3deg);
+  }
 
   img {
     width: 100%;
@@ -37,12 +58,10 @@ const EmptyCover = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    min-width: 160px;
-    max-width: 180px;
-    width: 160px;
+    width: 180px;
     height: 260px;
-    border: 1px solid #000;
     border-radius: 5px;
+    padding: 5px;
     
 `;
 
